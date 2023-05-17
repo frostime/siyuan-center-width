88843aea987592517ffe7d2d5f27a67dbb85b782
frostime <frostime@foxmail.com>
2023-05-17 20:38:58 +0800

    完成了基本功能

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,49 @@
 import { Plugin, Dialog } from "siyuan";
 import "./index.scss";
 
-const STORAGE_NAME = "menu-config";
-
 class ChangeWidthDialog extends Dialog {
 
     value: number;
 
-    constructor() {
+    constructor(plugin: PluginSample) {
         let dom = `
-        <input class="b3-slider fn__size200" id="centerWidth" max="100" min="50" step="1" type="range" value="70"/>
+        <div id="setting" style="margin: 1rem">
+            50%
+            <input
+                class="b3-slider fn__size200" id="centerWidth"
+                max="100" min="50" step="1" type="range" value="70"
+            />
+            100%
+            <br/>
+        </div>
         `
         super({
-            title: "更改中央面板宽度",
+            title: `更改中央面板宽度: ${plugin.width}%`,
             content: dom,
         });
+        let header: HTMLDivElement = this.element.querySelector('.b3-dialog__header');
+        header.style.textAlign = "center";
         this.element.querySelector("#centerWidth").addEventListener("input", (e) => {
-            console.log(e);
-            let width: number = parseInt((e.target as HTMLInputElement).value);
-            console.log(width);
+            plugin.width = parseInt((e.target as HTMLInputElement).value);
+            header.innerText = `更改中央面板宽度: ${plugin.width}%`;
+            document.documentElement.style.setProperty('--centerWidth', `${plugin.width}%`);
         });
     }
 }
 
 export default class PluginSample extends Plugin {
 
-    onload() {
-        this.data[STORAGE_NAME] = { readonlyText: "Readonly" };
+    width: number;
 
+    onload() {
+        this.width = 70;
+        document.documentElement.style.setProperty('--centerWidth', `${this.width}%`);
         const topBarElement = this.addTopBar({
-            icon: "iconEmoji",
+            icon: "iconCalendar",
             title: this.i18n.addTopBarIcon,
             position: "right",
             callback: () => {
-                let dialog = new ChangeWidthDialog();
+                let dialog = new ChangeWidthDialog(this);
             }
         });
     }
