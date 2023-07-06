bcd09f4be73b35eb17276a477eb7d978bc50a7ef
frostime <frostime@foxmail.com>
2023-07-06 12:52:08 +0800

    refactor: 使用 config 替代原来的 width 文件

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ class ChangeWidthDialog extends Dialog {
             title: `${plugin.i18n.title}: ${plugin.width}%`,
             content: dom,
             destroyCallback: () => {
-                plugin.saveData("width", plugin.width);
+                plugin.save();
                 console.log("Write width", plugin.width);
             }
         });
@@ -52,6 +52,7 @@ class ChangeWidthDialog extends Dialog {
 export default class WidthPlugin extends Plugin {
 
     width: number;
+    enableMobile: boolean;
     iconEle: HTMLElement
 
     icon: string = `<svg t="1684328935774" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1746" width="32" height="32"><path d="M180 176h-60c-4.4 0-8 3.6-8 8v656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V184c0-4.4-3.6-8-8-8z m724 0h-60c-4.4 0-8 3.6-8 8v656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V184c0-4.4-3.6-8-8-8zM785.3 504.3L657.7 403.6c-4.7-3.7-11.7-0.4-11.7 5.7V476H378v-62.8c0-6-7-9.4-11.7-5.7L238.7 508.3c-3.7 2.9-3.7 8.5 0 11.3l127.5 100.8c4.7 3.7 11.7 0.4 11.7-5.7V548h268v62.8c0 6 7 9.4 11.7 5.7l127.5-100.8c3.8-2.9 3.8-8.5 0.2-11.4z" p-id="1747"></path></svg>`
@@ -99,13 +100,30 @@ export default class WidthPlugin extends Plugin {
     }
 
     async load() {
-        let result = await this.loadData("width");
-        console.log("Load width", result);
-        if (result) {
-            this.width = result;
+        let config = await this.loadData("config");
+        console.log("Load config", config);
+        if (config) {
+            // this.width = result;
+            this.data['config'] = config;
+            this.width = config.width;
+            this.enableMobile = config.enableMobile;
         } else {
             this.width = 70;
+            this.enableMobile = false;
+            this.data['config'] = {
+                width: 70,
+                enableMobile: false
+            };
         }
+    }
+
+    async save() {
+        this.data['config'] = {
+            width: this.width,
+            enableMobile: this.enableMobile
+        };
+        await this.saveData("config", this.data['config']);
+        console.log("Save config", this.data['config']);
     }
 
     private checkFullWidth() {
