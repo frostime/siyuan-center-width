69ab18efef72f626730a4af04726ae9b9a9fc3ff
frostime <frostime@foxmail.com>
2023-10-08 12:06:54 +0800

    fix full width bug

--- src/index.ts
+++ src/index.ts
@@ -81,6 +81,7 @@ export default class WidthPlugin extends Plugin {
 
     observer: MutationObserver;
     onLoadProtyle: ({ detail }) => void;
+    onDestroyProtyle: ({ detail }) => void;
 
     icon: string = `<svg t="1684328935774" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1746" width="32" height="32"><path d="M180 176h-60c-4.4 0-8 3.6-8 8v656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V184c0-4.4-3.6-8-8-8z m724 0h-60c-4.4 0-8 3.6-8 8v656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V184c0-4.4-3.6-8-8-8zM785.3 504.3L657.7 403.6c-4.7-3.7-11.7-0.4-11.7 5.7V476H378v-62.8c0-6-7-9.4-11.7-5.7L238.7 508.3c-3.7 2.9-3.7 8.5 0 11.3l127.5 100.8c4.7 3.7 11.7 0.4 11.7-5.7V548h268v62.8c0 6 7 9.4 11.7 5.7l127.5-100.8c3.8-2.9 3.8-8.5 0.2-11.4z" p-id="1747"></path></svg>`
 
@@ -154,7 +155,12 @@ export default class WidthPlugin extends Plugin {
             });
         }).bind(this);
 
+        this.onDestroyProtyle = (({ detail }) => {
+
+        }).bind(this);
+
         this.eventBus.on("loaded-protyle", this.onLoadProtyle);
+        
 
         let forbidMobile = !this.enableMobile && getFrontend() === "mobile";
 
@@ -251,8 +257,17 @@ export default class WidthPlugin extends Plugin {
     updateWysiwygPadding(wysiwyg: WeakRef<HTMLElement>) {
         let ele = wysiwyg?.deref();
         if (ele) {
+            let parentElement = ele.parentElement;
+            if (!parentElement) {
+                return;
+            }
+            let fullWidth = parentElement.getAttribute("data-fullwidth");
+            if (fullWidth === 'true') {
+                //full width 下不需要设置 padding
+                return;
+            }
+
             let parentWidth = ele.parentElement.clientWidth;
-            
             //被关闭的 tab 的 parentElement 宽度为 0
             if (parentWidth === 0) {
                 parentWidth = ele?.parentElement?.parentElement?.parentElement?.clientWidth;
